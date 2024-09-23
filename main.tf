227580c8878eadcf6b409271bad16f591faade8f
Guilherme Luz <>
2024-09-23 16:43:20 -0300

    feat: create rds postgres

--- main.tf
+++ main.tf
@@ -95,6 +95,47 @@ data "aws_iam_role" "lab_role" {
   name = "LabRole"
 }
 
+resource "aws_db_instance" "postgres" {
+  allocated_storage    = 20
+  engine               = "postgres"
+  engine_version       = "16.3"
+  instance_class       = "db.t3.micro"
+  username             = "postgres"
+  password             = "postgres"
+  parameter_group_name = "default.postgres16"
+  db_subnet_group_name = aws_db_subnet_group.rds_subnet.name
+  skip_final_snapshot  = true
+  publicly_accessible  = false
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+
+  tags = {
+    Name = "postgres-db"
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-security-group"
+  vpc_id      = aws_vpc.eks_vpc.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds-security-group"
+  }
+}
+
 resource "aws_eks_cluster" "eks" {
   name     = var.cluster_name
   version  = "1.30"
@@ -137,43 +178,3 @@ resource "aws_db_subnet_group" "rds_subnet" {
   }
 }
 
-resource "aws_db_instance" "postgres" {
-  allocated_storage    = 20
-  engine               = "postgres"
-  engine_version       = "16.3"
-  instance_class       = "db.t3.micro"
-  username             = "postgres"
-  password             = "postgres"
-  parameter_group_name = "default.postgres16"
-  db_subnet_group_name = aws_db_subnet_group.rds_subnet.name
-  skip_final_snapshot  = true
-  publicly_accessible  = false
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
-
-  tags = {
-    Name = "postgres-db"
-  }
-}
-
-resource "aws_security_group" "rds_sg" {
-  name        = "rds-security-group"
-  vpc_id      = aws_vpc.eks_vpc.id
-
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"] 
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "rds-security-group"
-  }
-}
